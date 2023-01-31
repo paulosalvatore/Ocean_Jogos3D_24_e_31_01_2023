44c5efaabac0dea6edf1ae6391b7e7dd93d91d3e
Paulo Salvatore <salvatoregames@gmail.com>
2023-01-31 11:04:48 -0300

    Script `Cannon`: `InvokeRepeating` adicionado (WIP)

--- Assets/Cannon.cs
+++ Assets/Cannon.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 public class Cannon : MonoBehaviour
 {
     /*
-    1. A cada X segundos, executamos um método que cria um projétil novo
-    2. Assim que o projétil for criado, ativamos o GameObject para que o movimento seja iniciado
+    1. A cada X segundos, executa um método
+    2. Ao executar o método, cria um projétil novo
+    3. Assim que o projétil for criado, ativa o GameObject para que o movimento seja iniciado
     */
+
+    public float initialDelay = 1f;
+
+    public float repeatRate = 0.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("GenerateBullet", initialDelay, repeatRate);
     }
 
-    // Update is called once per frame
-    void Update()
+    void GenerateBullet()
     {
-        
+        print("GenerateBullet");
     }
 }
