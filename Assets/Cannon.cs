c874e1a21ba2767845a938e0ccc36d54be61da0b
Paulo Salvatore <salvatoregames@gmail.com>
2023-01-31 10:27:59 -0300

    Canhão criado (WIP)

---
+++ Assets/Cannon.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    /*
+    1. A cada X segundos, executamos um método que cria um projétil novo
+    2. Assim que o projétil for criado, ativamos o GameObject para que o movimento seja iniciado
+    */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
