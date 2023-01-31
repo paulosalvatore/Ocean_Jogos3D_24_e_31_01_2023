4e25fead8ab6a1a4d9dae101dd0d8152f4cb9f68
Paulo Salvatore <salvatoregames@gmail.com>
2023-01-31 11:34:52 -0300

    Implementação do `Cannon` finalizada

--- Assets/Cannon.cs
+++ Assets/Cannon.cs
@@ -22,6 +22,34 @@ public class Cannon : MonoBehaviour
 
     void GenerateBullet()
     {
-        print("GenerateBullet");
+        // Pegamos o primeiro objeto filho do Cannon, que no caso é o objeto Bullet
+        // Como os filhos de objetos ficam conectados a seu transform, pegamos
+        // o GameObject do objeto Bullet
+        var bulletOriginal = transform.GetChild(0).gameObject;
+        // print(bulletOriginal);
+
+        // Fazer uma cópia do bulletOriginal
+        // Para isso, usamos o método Instantiate, que recebe um objeto qualquer e duplica esse objeto
+        // Esse objeto recebido pode ser tanto um Prefab (que está na aba Projects) ou um GameObject,
+        // que está na cena.
+        // Assim que o Instantiate é executado, recebemos como retorno o objeto que foi clonado
+
+        var bulletClone = Instantiate(
+            bulletOriginal,
+            bulletOriginal.transform.position,
+            bulletOriginal.transform.rotation
+        );
+
+        // Utilizando o objeto clonado, executamos o SetActive(true), para que o objeto fique visível
+        // na cena
+        bulletClone.SetActive(true);
+
+        // print("GenerateBullet"); // Sugar Syntax -> Unity -> Debug.Log -> Console.WriteLine
+
+        // Console.WriteLine("Teste"); // C#
+
+        // Debug.Log("Teste"); // Unity
+        // Debug.LogWarning(); // Unity -> envia como Warning (amarelo)
+        // Debug.LogError(); // Unity -> envia como Error (vermelho)
     }
 }
